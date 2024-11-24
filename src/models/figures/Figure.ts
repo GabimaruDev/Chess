0b48487fa23d00dc92df4d02fcace051aa8cecf9
Gabimaru <goukilya5@gmail.com>
2024-11-24 23:02:50 +0700

    feat: Расставлены фигуры по местам + стиль фигуры

--- src/models/figures/Figure.ts
+++ src/models/figures/Figure.ts
@@ -1,3 +1,36 @@
+import { Colors } from "../Colors";
+import logo from "../../assets/black-bishop.png";
+import { Cell } from "../Cell";
+
+export enum FigureNames {
+    "FIGURE" = "Фигура",
+    "KING" = "Король",
+    "QUEEN" = "Ферзь",
+    "BISHOP" = "Слон",
+    "KNIGHT" = "Конь",
+    "ROOK" = "Ладья",
+    "PAWN" = "Пешка",
+}
+
 export class Figure {
-    
+    color: Colors;
+    logo: typeof logo | null;
+    cell: Cell;
+    name: FigureNames;
+    id: Number;
+
+    constructor(color: Colors, cell: Cell) {
+        this.color = color;
+        this.cell = cell;
+        this.cell.figure = this;
+        this.logo = null;
+        this.name = FigureNames.FIGURE;
+        this.id = cell.x;
+    }
+
+    canMove(cell: Cell): boolean {
+        return true;
+    }
+
+    moveFigure(target: Cell) {}
 }
