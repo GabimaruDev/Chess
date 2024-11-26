5d4e3b5123b39ba22e6a000f6238b0b651831a97
Gabimaru <goukilya5@gmail.com>
2024-11-26 08:15:37 +0700

    Передача хода

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react";
 import { Board } from "./models/Board";
 import BoardComponent from "./components/BoardComponent";
+import { Player } from "./models/Player";
+import { Colors } from "./models/Colors";
 
 function App() {
     const [board, setBoard] = useState(new Board());
+    const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE));
+    const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK));
+    const [currentPlayer, setCurrentPlayer] = useState<Player>(whitePlayer);
 
     useEffect(() => {
         restart();
@@ -14,11 +19,22 @@ function App() {
         newBoard.initCells();
         newBoard.addFigures();
         setBoard(newBoard);
+        setCurrentPlayer(whitePlayer);
+    }
+
+    function swapPlayer() {
+        setCurrentPlayer(currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer);
     }
 
     return (
         <div className="app">
-            <BoardComponent board={board} setBoard={setBoard} />
+            <h2>Ход игрока: {currentPlayer?.color == "black" ? "чёрный" : "белый"}</h2>
+            <BoardComponent
+                board={board}
+                setBoard={setBoard}
+                currentPlayer={currentPlayer}
+                swapPlayer={swapPlayer}
+            />
         </div>
     );
 }
