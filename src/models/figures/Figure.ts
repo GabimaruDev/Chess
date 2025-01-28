f8a9670ef3eeca88fd10110d676a18a0f3f9c049
Stas Kobles <sskobles@gmail.com>
2025-01-28 19:47:04 +0700

    Исправление рекурсии

--- src/models/figures/Figure.ts
+++ src/models/figures/Figure.ts
@@ -2,40 +2,40 @@ import { Colors } from "../Colors";
 import { Cell } from "../Cell";
 
 export enum FigureNames {
-    "FIGURE" = "Фигура",
-    "KING" = "Король",
-    "QUEEN" = "Ферзь",
-    "BISHOP" = "Слон",
-    "KNIGHT" = "Конь",
-    "ROOK" = "Ладья",
-    "PAWN" = "Пешка",
+  "FIGURE" = "Фигура",
+  "KING" = "Король",
+  "QUEEN" = "Ферзь",
+  "BISHOP" = "Слон",
+  "KNIGHT" = "Конь",
+  "ROOK" = "Ладья",
+  "PAWN" = "Пешка",
 }
 
 export class Figure {
-    color: Colors;
-    logo: string | null;
-    cell: Cell;
-    name: FigureNames;
-    id: number;
-    isFirstStep: boolean | null = null;
+  color: Colors;
+  logo: string | null;
+  cell: Cell;
+  name: FigureNames;
+  id: number;
+  isFirstStep: boolean | null = null;
 
-    constructor(color: Colors, cell: Cell) {
-        this.color = color;
-        this.cell = cell;
-        this.cell.figure = this;
-        this.logo = null;
-        this.name = FigureNames.FIGURE;
-        this.id = Math.random();
-    }
+  constructor(color: Colors, cell: Cell) {
+    this.color = color;
+    this.cell = cell;
+    this.cell.figure = this;
+    this.logo = null;
+    this.name = FigureNames.FIGURE;
+    this.id = Math.random();
+  }
 
-    public canMove(target: Cell): boolean {
-        if (target.figure?.color === this.color) return false;
-        return true;
-    }
+  public canMove(target: Cell, checkingForAttack = false): boolean {
+    if (target.figure?.color === this.color) return false;
+    return true;
+  }
 
-    public moveFigure() {}
+  public moveFigure() {}
 
-    public firstStep(): boolean | null {
-        return this.isFirstStep;
-    }
+  public firstStep(): boolean | null {
+    return this.isFirstStep;
+  }
 }
--- src/models/figures/King.ts
+++ src/models/figures/King.ts
@@ -5,29 +5,35 @@ import blackLogo from "../../assets/min-black-king.png";
 import yellowLogo from "../../assets/min-yellow-king.png";
 
 export class King extends Figure {
-    isFirstStep: boolean = true;
+  isFirstStep: boolean = true;
 
-    constructor(color: Colors, cell: Cell) {
-        super(color, cell);
-        this.logo = color === Colors.BLACK ? blackLogo : yellowLogo;
-        this.name = FigureNames.KING;
+  constructor(color: Colors, cell: Cell) {
+    super(color, cell);
+    this.logo = color === Colors.BLACK ? blackLogo : yellowLogo;
+    this.name = FigureNames.KING;
+  }
+
+  canMove(target: Cell, checkingForAttack = false): boolean {
+    if (!super.canMove(target)) return false;
+    if (this.cell.board.castling(target, this.color)) return true;
+
+    if (
+      !checkingForAttack &&
+      this.cell.board.isCellUnderAttack(target, this.color)
+    ) {
+      return false;
     }
 
-    canMove(target: Cell): boolean {
-        if (!super.canMove(target)) return false;
-        if (this.cell.board.castling(target, this.color)) return true;
-        if (this.cell.board.isCellUnderAttack(target, this.color)) return false;
+    const dx = Math.abs(this.cell.x - target.x);
+    const dy = Math.abs(this.cell.y - target.y);
+    return dx <= 1 && dy <= 1;
+  }
 
-        const dx = Math.abs(this.cell.x - target.x);
-        const dy = Math.abs(this.cell.y - target.y);
-        return dx <= 1 && dy <= 1;
-    }
+  firstStep(): boolean {
+    return this.isFirstStep;
+  }
 
-    firstStep(): boolean {
-        return this.isFirstStep;
-    }
-
-    moveFigure(): void {
-        this.isFirstStep = false;
-    }
+  moveFigure(): void {
+    this.isFirstStep = false;
+  }
 }
