b8beb6d317ffd6e45fd853e76c33f416dd56a0d3
Ilya Zhukov <goukilya5@gmail.com>
2025-08-27 21:06:52 +0700

    fix: timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -41,23 +41,39 @@ const Timer: FC<TimerProps> = (props) => {
         }
     }, [currentPlayer, startTimer, isPaused, stopTimer]);
 
-    function decrementBlackTimer() {
+    const decrementBlackTimer = () => {
         setBlackTime((prev) => {
             return prev > 0 ? prev - 0.1 : 0;
         });
-    }
+    };
 
-    function decrementWhiteTimer() {
+    const decrementWhiteTimer = () => {
         setWhiteTime((prev) => {
             return prev > 0 ? prev - 0.1 : 0;
         });
-    }
+    };
 
-    function handleRestart() {
-        setBlackTime(300);
-        setWhiteTime(300);
+    const formatTime = (seconds: number): string => {
+        const totalSeconds = Math.floor(seconds);
+        const tenths = Math.round((seconds % 1) * 10);
+        const minutes = Number(String(Math.floor(totalSeconds / 60)).padStart(1, "0"));
+        const remainingSeconds = String(totalSeconds % 60).padStart(2, "0");
+        return minutes == 0
+            ? `${totalSeconds}.${tenths == 10 ? 0 : String(tenths)}`
+            : minutes == -1
+            ? "0.0"
+            : `${minutes}:${remainingSeconds}`;
+    };
+
+    const setTime = (time: number) => {
+        setBlackTime(time);
+        setWhiteTime(time);
+    };
+
+    const handleRestart = () => {
+        setTime(600);
         restart();
-    }
+    };
 
     const renderModal = (title: string, body: string, footer: JSX.Element) => (
         <ModalWindow title={title} body={body} footer={footer} />
@@ -66,24 +82,24 @@ const Timer: FC<TimerProps> = (props) => {
     return (
         <div className="timer">
             <div className="timer__time">
-                <h2>Чёрные - {blackTime.toFixed(1)}</h2>
-                <h2>{whiteTime.toFixed(1)} - Белые</h2>
+                <h2>
+                    Белые -{" "}
+                    <span className={whiteTime < 15 && whiteTime > 0.01 ? "timer__low-time" : ""}>
+                        {formatTime(whiteTime)}
+                    </span>
+                </h2>
+                <h2>
+                    <span className={blackTime < 15 && blackTime > 0.01 ? "timer__low-time" : ""}>
+                        {formatTime(blackTime)}
+                    </span>{" "}
+                    - Чёрные
+                </h2>
             </div>
             <div>
                 <button className="btn" onClick={handleRestart}>
                     Новая игра
                 </button>
             </div>
-            {(blackTime < 0.1 || whiteTime < 0.1) &&
-                !gameState.isCheckmate &&
-                !gameState.isStalemate &&
-                renderModal(
-                    "Время вышло!",
-                    "Сыграете ещё одну игру?",
-                    <button className="btn" onClick={handleRestart}>
-                        Новая игра
-                    </button>
-                )}
             {gameState.isCheckmate &&
                 renderModal(
                     `Мат! ${
@@ -102,16 +118,23 @@ const Timer: FC<TimerProps> = (props) => {
                         Новая игра
                     </button>
                 )}
+            {(blackTime < 0.01 || whiteTime < 0.01) &&
+                renderModal(
+                    `Время ${blackTime < 0.01 ? "чёрных" : "белых"} вышло!`,
+                    "Сыграете ещё одну игру?",
+                    <button className="btn" onClick={handleRestart}>
+                        Новая игра
+                    </button>
+                )}
             {isStartGame &&
                 renderModal(
                     "Классические шахматы",
-                    "Нажмите на кнопку, если готовы начать играть",
+                    `Нажмите на кнопку, если готовы начать играть`,
                     <button
                         className="btn"
                         onClick={() => {
                             initGame();
-                            setBlackTime(300);
-                            setWhiteTime(300);
+                            setTime(600);
                         }}
                     >
                         Играть
